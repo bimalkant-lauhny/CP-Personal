2607c44dbdca2e5c83c35aa2f0f8fb40abd44ed8
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-06-18 10:01:39 +0530

    1042 Toeplitz Matrix

---
+++ lintcode/1042_toeplitz_matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    /**
+     * @param matrix: the given matrix
+     * @return: True if and only if the matrix is Toeplitz
+     */
+    bool isToeplitzMatrix(vector<vector<int>> &matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        for (int i=1; i<rows; ++i) {
+            for (int j=1; j<cols; ++j) {
+                if (matrix[i][j] != matrix[i-1][j-1]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
