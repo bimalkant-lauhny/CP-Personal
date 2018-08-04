d84d0054ff0b09be9fbd0823feb5a32ad10cfe3c
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-08-04 23:02:56 +0530

    Binary Tree Inorder

---
+++ lintcode/67_binary_tree_inorder_traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    vector<int> inorderTraversal(TreeNode * root) {
+        vector <int> res;
+        if (!root)
+            return res;
+            
+        stack<TreeNode*> s;
+        TreeNode *node = root;
+        
+        while (!s.empty() || node) {
+            if (node) {
+                s.push(node);
+                node = node->left;
+            } else {
+                node = s.top();
+                s.pop();
+                res.push_back(node->val);
+                node = node->right;
+            }
+        }
+        
+        return res;
+    }
+};
